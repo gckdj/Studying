4f6570a6945e89b34f54298176e9fd1947db6bce
selfMade <88639900+peaks26@users.noreply.github.com>
2022-04-17 23:58:51 +0900

    애플리케이션 컨텍스트 컨테이너 수강

--- Spring/kimyounghan(junior)/core/src/main/java/hello/core/AppConfig.java
+++ Spring/kimyounghan(junior)/core/src/main/java/hello/core/AppConfig.java
@@ -2,30 +2,37 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDidcountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceimpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     /* appConfig에서 반환하는 구현체만 변경하면 기존 코드는 수정할 필요가 없음. */
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceimpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDidcountPolicy();
     }
 }
--- Spring/kimyounghan(junior)/core/src/main/java/hello/core/OrderApp.java
+++ Spring/kimyounghan(junior)/core/src/main/java/hello/core/OrderApp.java
@@ -5,11 +5,17 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceimpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl( new MemoryMemberRepository());
-        OrderService orderService = new OrderServiceimpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//        MemberService memberService = new MemberServiceImpl( new MemoryMemberRepository());
+//        OrderService orderService = new OrderServiceimpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -18,5 +24,6 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order);
         System.out.println("order = " + order.calculatePrice());
+
     }
 }

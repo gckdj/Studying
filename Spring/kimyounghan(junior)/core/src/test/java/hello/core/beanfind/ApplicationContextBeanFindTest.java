5cf6aca9928573a08c6a3fb1a43605cc1e69eed4
selfMade <88639900+peaks26@users.noreply.github.com>
2022-04-20 22:55:36 +0900

    BeanDefinitionTest, SingletonService

--- Spring/kimyounghan(junior)/core/src/test/java/hello/core/beanfind/ApplicationContextBeanFindTest.java
+++ Spring/kimyounghan(junior)/core/src/test/java/hello/core/beanfind/ApplicationContextBeanFindTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextBeanFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 요류가 발생한다.")
@@ -39,24 +39,11 @@ public class ApplicationContextBeanFindTest {
     @DisplayName("특정 타입의 모두 조회하기")
     void findAllBeanByType() {
         Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
-        for (String key : beansOfType.keySet())  {
+        for (String key : beansOfType.keySet()) {
             System.out.println("key + = " + key + " value = " + beansOfType.get(key));
         }
 
         System.out.println("beansOfType = " + beansOfType);
         assertThat(beansOfType.size()).isEqualTo(2);
-    }
-
-    @Configuration
-    static class SameBeanConfig {
-        @Bean
-        public MemberRepository memberRepository() {
-            return new MemoryMemberRepository();
-        }
-
-        @Bean
-        public MemberRepository memberRepository2() {
-            return new MemoryMemberRepository();
-        }
     }
 }

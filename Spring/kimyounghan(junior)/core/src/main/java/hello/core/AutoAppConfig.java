758674bc6615146edeae7450fb4362a26e16cb84
selfMade <88639900+peaks26@users.noreply.github.com>
2022-04-25 20:46:22 +0900

    Component

--- Spring/kimyounghan(junior)/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring/kimyounghan(junior)/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // 탐색할 패키지의 시작위치 지정
+        basePackages = "hello.core.member",
+        // 탐색할 클래스 시작위치 지정 ※ 지정하지 않을경우 @ComponentScan 이 붙은 설정정보 클래스의 패키지가 시작위치가 된다.
+        // 프로젝트의 최상단에 두는 것이 관례
+        basePackageClasses = AutoAppConfig.class,
         // 예제에서 사용하던 Configuration 제외
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )

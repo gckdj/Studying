40083dc027e473dcfa9a84ee4d7eb52a2eb430cc
kdj <88639900+djgohigh@users.noreply.github.com>
2022-05-07 20:59:10 +0900

    빈 초기화, 소멸 시 특정 메서드를 동작하는 방법
    1. 스프링 인터페이스에 의존
    2. @Bean 에 메서드 명시
    3. 자바 표준명세 이용(@PostConstuct, @PreDestory)

--- Spring/kimyounghan(junior)/core/src/test/java/hello/core/lifecycle/BenaLifeCycleTest.java
+++ Spring/kimyounghan(junior)/core/src/test/java/hello/core/lifecycle/BenaLifeCycleTest.java
@@ -17,6 +17,11 @@ public class BenaLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
         
+        /* Bean에 최초, 종료 시 실행할 메서드를 지정할 수 있음 */
+        // 장점 : 메서드 이름 자유롭게 작명, 스프링 빈이 스프링 인터페이스에 의존하지 않음, 코드를 고칠 수 없는 외부 라이브러리도 초기화, 종료 가능
+        // destroyMethod -> 기본값이 '(inferred)' 의미는 추론, 이름 그대로 종료메서드를 추론해서 호출한다.
+        // @Bean(initMethod = "init", destroyMethod = "close")
+        
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
@@ -29,5 +34,4 @@ public class BenaLifeCycleTest {
     // 스프링은 의존관계가 주입되면 스프링 빈에 '콜백메서드'를 통해서 초기화 시점을 알려주는 다양한 기능을 제공
     // 스프링 컨테이너가 종료되기 직전에 소멸 콜백을 줌으로써 안전하게 종료작업 진행
     // 객체의 생성과 초기화는 분리하는 것이 좋다. (-> 생성자 내에서는 객체 내에서의 값을 지정하는 정도로 하는 것이 좋다.)
-    
 }
--- Spring/kimyounghan(junior)/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ Spring/kimyounghan(junior)/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient /* implements InitializingBean, DisposableBean */ {
     
     private String url;
     
-    public NetworkClient() {
-        System.out.println("생성자 호출, url = " + url);
+    public NetworkClient()  {
         connect();
-        call("초기화 연결 메시지");
     }
     
     public void setUrl(String url) {
@@ -25,5 +29,60 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+    /* 스프링 인터페이스에 의존하는 메서드 구현하는 방법 시작 */
+    
+    // InitailizingBean 구현 시 추가되는 메서드
+    // 스프링 시작시 빈들이 등록되고 나면 실행되는 메서드
+    // @Override
+    // public void afterPropertiesSet() throws Exception {
+    //     connect();
+    //     call("초기화 연결 메시지");
+    // }
+    
+    // DisposableBean 구현 시 추가되는 메서드
+    // 스프링 종료 전에 실행되는 메서드
+    // @Override
+    // public void destroy() throws Exception {
+    //     System.out.println("NetworkClient.destroy");
+    //     disconnect();
+    // }
+    
+    // 스프링 전용 인터페이스를 의존하고 있어, 초기화 또는 소멸 메서드의 이름을 변경할 수 없다.
+    // 초창기에 나온 방법이고, 현재에는 거의 사용하지 않는다.
+    
+    /* 스프링 인터페이스에 의존하는 메서드 구현하는 방법 끝 */
+    
+    /* 빈에 메서드를 명시하는 방법 시작 */
+    
+    // public void init() {
+    //     System.out.println("NetworkClient.init()");
+    //     connect();
+    //     call("초기화 연결 메시지");
+    // }
+    
+    // public void close() throws Exception {
+    //     System.out.println("NetworkClient.close()");
+    //     disconnect();
+    // }
+    
+    /* 빈에 메서드를 명시하는 방법 종료 */
+    
+    /* @PostContstruct, @PreDestroy 에노테이션을 이용하는 방법 시작 */
+    
+    // 자바진영에서 공식적으로 지원하는 에노테이션, 스프링이 아닌 다른 프레임워크, 컨테이너에서도 동작
+    // 유일한 단점은 외부 라이브러리에는 적용할 수가 없다. 외부라이브러리를 적용할 경우에는 빈에 메서드(initMethod, destroyMethod)를 명시하는 방법을 이용
+    
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init()");
+        connect();
+        call("초기화 연결 메시지");
+    }
+    
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close()");
+        disconnect();
     }
 }

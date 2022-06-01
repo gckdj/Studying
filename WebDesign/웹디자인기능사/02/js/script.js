bbbe18eac6c2da4fe727e53776a039d698c5f6b4
selfMade <88639900+peaks26@users.noreply.github.com>
2022-06-01 18:11:46 +0900

    02 end

--- WebDesign/웹디자인기능사/02/js/script.js
+++ WebDesign/웹디자인기능사/02/js/script.js
@@ -1,31 +1,42 @@
 $(function () {
-
     // 네비게이션
     $("header nav ul.gnb > li").mouseenter(function () {
         $(".nav_bg, ul.sub").stop().slideDown();
     });
-    
+
     $("header nav ul.gnb > li").mouseleave(function () {
         $(".nav_bg, ul.sub").stop().slideUp();
     });
-    
     // 슬라이드
-});
+    var slideId = 0;
+    setInterval(function () {
+        if (slideId < 2) {
+            slideId++;
+        } else {
+            slideId = 0;
+        }
 
-var slideId = 0;
-setInterval(function () {
-    if (slideId < 2) {
-        slideId++;
-    } else {
-        slideId = 0;
-    }
+        $("ul.slide li").eq(slideId).siblings().animate(
+            {
+                left: "-1200px",
+            },
+            500
+        );
+
+        $("ul.slide li").eq(slideId).animate(
+            {
+                left: "0",
+            },
+            500
+        );
+    }, 3000);
     
-    $("ul.slide li").eq(slideId).siblings().animate({
-        left: "-1200px"
-    }, 500);
+    $('.notice li:nth-child(1)').click(function () {
+        console.log("11");
+        $('.popUpBg, .popUpMbox').show();
+    });
     
-    $("ul.slide li").eq(slideId).animate({
-        left: "0"
-    }, 500);
-    
-}, 3000);
+    $('#close').click(function () {
+        $('.popUpBg, .popUpMbox').hide();
+    });
+});

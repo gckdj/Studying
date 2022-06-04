af76a9ff013d4236326d9885fc4ad32fa960177d
selfMade <88639900+peaks26@users.noreply.github.com>
2022-06-04 23:16:11 +0900

    .
    .

--- WebDesign/웹디자인기능사/03/js/script.js
+++ WebDesign/웹디자인기능사/03/js/script.js
@@ -1,6 +1,16 @@
 $(function () {
-    $('.menu > li').mouseover(function() {
-    console.log("222")
-        $(this).find(ul).show();
+    $('li.lv1').mouseenter(function() {
+        $(this).find('ul.lv2').stop().slideDown();
     });
+    
+    $('li.lv1').mouseleave(function() {
+        $('ul.lv2').stop().slideUp();
+    });
+    
+    $('.slide:nth-child(2)').hide();
+    $('.slide:nth-child(3)').hide();
+    setInterval(function () {
+        $('.slide:first').fadeOut(1500).next().fadeIn(1500);
+        $('.slide:first').appendTo('.slideWrap');
+    }, 3000);
 });

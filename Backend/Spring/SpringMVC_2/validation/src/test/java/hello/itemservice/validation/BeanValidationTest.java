5e3cab1a27766913f0f286e5e15332db9eaf120c
kdj <88639900+djgohigh@users.noreply.github.com>
2022-06-26 23:57:16 +0900

    BeanValidation V3 프로젝트 준비

--- Backend/Spring/SpringMVC_2/validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
+++ Backend/Spring/SpringMVC_2/validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -3,9 +3,11 @@ package hello.itemservice.validation;
 import hello.itemservice.domain.item.Item;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Set;
 
 public class BeanValidationTest {
 
@@ -13,6 +15,7 @@ public class BeanValidationTest {
     @Test
     void beanValidation() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        //검증기 생성
         Validator validator = factory.getValidator();
 
         Item item = new Item();
@@ -20,6 +23,19 @@ public class BeanValidationTest {
         item.setPrice(0);
         item.setQuantity(10000);
 
+        //생성된 검증기에 객체를 넣고 결과를 Set 에 받는다
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
 
+        //result
+        //violation = ConstraintViolationImpl{interpolatedMessage='1000에서 1000000 사이여야 합니다', propertyPath=price, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{org.hibernate.validator.constraints.Range.message}'}
+        //violation = 1000에서 1000000 사이여야 합니다
+        //violation = ConstraintViolationImpl{interpolatedMessage='공백일 수 없습니다', propertyPath=itemName, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{javax.validation.constraints.NotBlank.message}'}
+        //violation = 공백일 수 없습니다
+        //violation = ConstraintViolationImpl{interpolatedMessage='9999 이하여야 합니다', propertyPath=quantity, rootBeanClass=class hello.itemservice.domain.item.Item, messageTemplate='{javax.validation.constraints.Max.message}'}
+        //violation = 9999 이하여야 합니다
     }
 }

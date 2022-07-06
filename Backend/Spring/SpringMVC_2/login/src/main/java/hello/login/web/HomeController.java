f30b22b6b3aaee98c756504ba00a3a5012091fac
kdj <88639900+djgohigh@users.noreply.github.com>
2022-07-06 20:08:59 +0900

    서블릿 http 세션2

--- Backend/Spring/SpringMVC_2/login/src/main/java/hello/login/web/HomeController.java
+++ Backend/Spring/SpringMVC_2/login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -26,7 +28,6 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/")
     // 요청정보에서 쿠키 값을 받아와도 되지만 스프링에서 어노테이션으로 지원
 //    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
 //
@@ -46,21 +47,53 @@ public class HomeController {
 //        return "loginHome";
 //    }
 
-    public String homeLoginV2(HttpServletRequest request, Model model) {
+//    public String homeLoginV2(HttpServletRequest request, Model model) {
+//
+//        // 세션관리자에 저장된 회원정보 조회
+//        // 요청에서 sessionId 로 표기된 쿠키를 찾고 있다면, 해당 sessionId 가 key 인 value 값(member 객체) 반환
+//        Member member = (Member) sessionManager.getSession(request);
+//
+//        // 로그인
+//        if (member == null) {
+//            return "home";
+//        }
+//
+//        model.addAttribute("member", member);
+//        return "loginHome";
+//
+//        // mySessionId : afd49c74-f4ed-474d-b472-77edd8aac4f7
+//        // 당연히도 서블릿에서도 세션을 지원한다.
+//    }
 
-        // 세션관리자에 저장된 회원정보 조회
-        // 요청에서 sessionId 로 표기된 쿠키를 찾고 있다면, 해당 sessionId 가 key 인 value 값(member 객체) 반환
-        Member member = (Member) sessionManager.getSession(request);
+//    public String homeLoginV3(HttpServletRequest request, Model model) {
+//        // true로 해버리면 접속만 해도 세션이 생성되기에 false 처리
+//        HttpSession session = request.getSession(false);
+//
+//        // 세션이 null 이거나
+//        if (session == null) {
+//            return "home";
+//        }
+//
+//        Member loginMember= (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+//
+//        // 찾은 세션내에서 로그인 멤버가 없으면 홈으로
+//        if (loginMember == null) {
+//            return "home";
+//        }
+//
+//        // 예외상황에 걸리지 않으면 모델에 속성설정
+//        model.addAttribute("member", loginMember);
+//        return "loginHome";
+//   }
 
-        // 로그인
-        if (member == null) {
+    // 스프링이 제공하는 세션기능을 사용한 로직
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        if (loginMember == null) {
             return "home";
         }
 
-        model.addAttribute("member", member);
+        model.addAttribute("member", loginMember);
         return "loginHome";
-
-        // mySessionId : afd49c74-f4ed-474d-b472-77edd8aac4f7
-        // 당연히도 서블릿에서도 세션을 지원한다.
     }
 }
---
+++ Backend/Spring/SpringMVC_2/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,8 @@
+package hello.login.web;
+
+// 인터페이스나, 추상클래스로 만드는 것이 더 좋다.
+// 이 클래스로 객체를 생성해서 사용할 일이 없음.
+public class SessionConst {
+
+    public static final String LOGIN_MEMBER = "LoginMember";
+}

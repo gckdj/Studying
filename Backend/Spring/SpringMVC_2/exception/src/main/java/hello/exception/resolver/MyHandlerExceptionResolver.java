145927c28791a83597e89604b0ee8b6c47212652
kdj <88639900+djgohigh@users.noreply.github.com>
2022-07-15 20:39:03 +0900

    HandlerExceptionResolver 시작

---
+++ Backend/Spring/SpringMVC_2/exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,35 @@
+package hello.exception.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView();
+            }
+        } catch (IOException e) {
+            log.error("resolver ex", e);
+        }
+        return null;
+
+        //{
+        //    "timestamp": "2022-07-15T11:38:04.689+00:00",
+        //    "status": 400,
+        //    "error": "Bad Request",
+        //    "exception": "java.lang.IllegalArgumentException",
+        //    "path": "/api/members/bad"
+        //}
+    }
+}

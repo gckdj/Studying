bf722bbc80473c9c74841bd796bc676482a0e15c
kdj <88639900+djgohigh@users.noreply.github.com>
2022-07-26 23:02:23 +0900

    Annotation ExceptionHandler

--- Backend/Spring/SpringMVC_2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ Backend/Spring/SpringMVC_2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -2,15 +2,17 @@ package hello.exception.api;
 
 import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@ Slf4j
-@ RestController
+@Slf4j
+@RestController
 public class ApiExceptionV2Controller {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -20,13 +22,15 @@ public class ApiExceptionV2Controller {
         return new ErrorResult("BAD", e.getMessage());
     }
 
+    // 컨트롤러 호출과 거의 동일
     @ExceptionHandler
     public ResponseEntity<ErrorResult> userExHandle(UserException e) {
         log.error("[exceptionHandle] ex", e);
-        ErrorResult  errorResult = new ErrorResult("USER-EX", e.getMessage());
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
         return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
+    // 위에서 매핑한 예외에서 처리하지 못하면 최상위 예외인 Exception에서 처리
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
     public ErrorResult exHandle(Exception e) {
@@ -34,6 +38,12 @@ public class ApiExceptionV2Controller {
         return new ErrorResult("EX", "내부 오류");
     }
 
+    //{
+    //    "code": "EX",
+    //    "message": "내부 오류"
+    //}
+
+    // 컨트롤러에서 매핑된 예외는 다른 컨트롤러까지 영행을 주지 않고 현재의 클래스에만 영향
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {

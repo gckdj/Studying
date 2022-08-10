cb6fb0d77ef48db769674f622d376380152a1855
kdj <88639900+djgohigh@users.noreply.github.com>
2022-08-10 23:25:39 +0900

    con

--- Backend/Spring/SpringDB-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ Backend/Spring/SpringDB-1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -4,9 +4,7 @@ import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -18,8 +16,52 @@ public class MemberRepositoryV0 {
         Connection con = null;
         PreparedStatement pstmt = null;
 
-        con = getConnection();
-        con.prepareStatement()
+
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // 이렇게 처리하면 둘 다 동시에 처리가 안될 수 있음
+            // pstmt.close(); // Exception
+            // con.close();
+
+            // 쿼리 실행 후 쿼리정리
+            // 쿼리 종료시에는 역순으로..
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
     }
 
     private Connection getConnection() throws SQLException {

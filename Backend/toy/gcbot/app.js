17a580bc5212585834e08a8d0f9c095c651e16c1
kdj <88639900+djgohigh@users.noreply.github.com>
2022-08-17 08:03:41 +0900

    .

--- Backend/toy/gcbot/app.js
+++ Backend/toy/gcbot/app.js
@@ -1,10 +1,40 @@
+const { REST, Routes } = require('discord.js');
 const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token, client_id } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+  
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+  
+	if (interaction.commandName === 'gc') {
+		await interaction.reply('jm!');
+	}
 });
 
 client.login(token);
+
+const commands = [
+  {
+    name: 'gc',
+    description: 'test!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(client_id), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();

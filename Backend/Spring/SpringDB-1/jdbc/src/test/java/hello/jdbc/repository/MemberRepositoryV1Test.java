ab6e1241e6b31738fbd3fd90d18fd7cebb32d92c
kdj <88639900+djgohigh@users.noreply.github.com>
2022-08-20 09:01:24 +0900

    Update MemberRepositoryV1Test.java

--- Backend/Spring/SpringDB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ Backend/Spring/SpringDB-1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,50 +1,25 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
-
+import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 @Slf4j
 class MemberRepositoryV1Test {
 
-    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    MemberRepositoryV1 repository;
 
-    @Test
-    void crud() throws SQLException {
-        Member member = new Member("memberV5", 15000);
-        repository.save(member);
+    @BeforeEach
+    void beforeEach() throws Exception {
 
-        // findById
-        Member findMember = repository.findById(member.getMemberId());
-        log.info("findMember={}", findMember);
-        // member 객체의 참조값이 보이는 것이 아니라 실 데이터가 보이는 이유는 롬복이 toString()을 통해 오버라이딩 해주기 때문
-        log.info("member == findMember {}", member == findMember);
-        log.info("member equals findMember {}", member.equals(findMember));
-        assertThat(findMember).isEqualTo(member);
-        //21:27:35.934 [main] INFO hello.jdbc.repository.MemberRepositoryV0Test - findMember=Member(memberId=memberV2, money=15000)
-        //21:27:35.943 [main] INFO hello.jdbc.repository.MemberRepositoryV0Test - member == findMember false
-        //21:27:35.943 [main] INFO hello.jdbc.repository.MemberRepositoryV0Test - member equals findMember true
-
-        // 특정 회원의 money 변경하기
-        // update : money 10000 -> 20000
-        repository.update(member.getMemberId(), 20000);
-        Member updatedMember = repository.findById(member.getMemberId());
-        assertThat(updatedMember.getMoney()).isEqualTo(20000);
-
-        // delete
-        // 테스트를 위해 삭제하는 로직을 구성해도 이 이전의 로직에서 다른 예외가 터져버린다면, 삭제하는 로직이 실행이 안된다.
-        // 테스트 간에는 이 부분을 유의.. -> 궁극적인 방법이 아님.
-        repository.delete(member.getMemberId());
-        // 삭제되었기 때문에 예외터짐
-        Member deletedMember = repository.findById(member.getMemberId());
-        // 해당 데이터가 없는 경우에는 NoSuchElementException 예외이용
-        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
-                .isInstanceOf(NoSuchElementException.class);
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        repository = new MemberRepositoryV1(dataSource)
     }
-}

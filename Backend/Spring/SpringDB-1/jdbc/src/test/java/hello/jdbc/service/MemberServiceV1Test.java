19e8dabef78f319a7389bd1a5b817f490658945a
kdj <88639900+djgohigh@users.noreply.github.com>
2022-08-23 23:04:02 +0900

    트랜잭션 적용 - 1

---
+++ Backend/Spring/SpringDB-1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,34 @@
+package hello.jdbc.service;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제발생
+ */
+class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() {
+
+    }
+}

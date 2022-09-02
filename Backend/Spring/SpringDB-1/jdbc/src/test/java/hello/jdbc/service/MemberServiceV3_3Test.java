6cd0b11c4ca09b23d079d1987510ad921c6e7a3a
kdj <88639900+djgohigh@users.noreply.github.com>
2022-09-02 23:34:44 +0900

    aop  적용

--- Backend/Spring/SpringDB-1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
+++ Backend/Spring/SpringDB-1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
@@ -29,23 +33,57 @@ class MemberServiceV3_3Test {
     public static final String MEMBER_B = "memberEx";
     public static final String MEMBER_EX = "ex";
 
+    @Autowired
     private MemberRepositoryV3 memberRepository;
+
+    @Autowired
     private MemberServiceV3_3 memberService;
 
-    @BeforeEach
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+
+    /*@BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         memberRepository = new MemberRepositoryV3(dataSource);
         memberService = new MemberServiceV3_3(memberRepository);
         // 현재 상태로 테스트를 돌리면 오류발생 -> 스프링 컨테이너 없이 사용했기 때문임.(순수한 자바코드)
         // 트랜잭션 AOP를 적용하기 위해서는 스프링 컨테이너에 빈으로 등록되어야 한다. @SpringBootTest
-    }
+    }*/
 
     @AfterEach
     void after() throws SQLException {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    void AppCheck() {
+        log.info("memberService class = {}", memberService.getClass());
+        log.info("memberRepository class = {}", memberRepository.getClass());
+        //2022-09-02 23:29:39.813  INFO 7767 --- [           main] h.jdbc.service.MemberServiceV3_3Test     : memberService class = class hello.jdbc.service.MemberServiceV3_3$$EnhancerBySpringCGLIB$$bd8cd4d8
+        //2022-09-02 23:29:39.813  INFO 7767 --- [           main] h.jdbc.service.MemberServiceV3_3Test     : memberRepository class = class hello.jdbc.repository.MemberRepositoryV3
     }
 
     @Test
@@ -67,4 +105,6 @@ class MemberServiceV3_3Test {
         assertThat(findMemberA.getMoney()).isEqualTo(10000);
         assertThat(findmemberEx.getMoney()).isEqualTo(10000);
     }
+
+    // @Transactional 주석처리한다면 오류처리남 -> 트랜잭션이 적용이 안되어서 롤백처리가 안되었기 때문
 }

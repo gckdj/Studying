771aba83ddb4957616ddd794b731f3d0662b0aa6
kdj <88639900+djgohigh@users.noreply.github.com>
2022-09-29 23:03:20 +0900

    스프링 데이터 JPA 적용2

---
+++ Backend/Spring/SpringDB-2/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
@@ -0,0 +1,41 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepositoryV2 implements ItemRepository {
+
+    private final SpringDataJpaItemRepository repository;
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+         
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return null;
+    }
+}

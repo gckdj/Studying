d09917fddc30b17b4c8edfbd32a420af6e0fd93e
kdj <88639900+djgohigh@users.noreply.github.com>
2022-10-16 23:07:38 +0900

    스프링 트랜잭션 전파 - 외,내부 롤백

--- Backend/Spring/SpringDB-2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ Backend/Spring/SpringDB-2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +9,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
@@ -95,6 +98,70 @@ public class BasicTxTest {
         log.info("내부 트랜잭션 커밋");
 
         // 하나의 트랜잭션으로 묶여서 커밋 코드가 있어도 커밋하지 않는다.
+        // 내부 트랜잭션 코드는 실질적인 기능을 하는 것이 없다.
         txManager.commit(inner);
+    }
+
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+
+        //2022-10-16 22:47:47.773  INFO 1560 --- [           main] hello.springtx.propagation.BasicTxTest   : 내부 트랜잭션 커밋
+        //2022-10-16 22:47:47.773  INFO 1560 --- [           main] hello.springtx.propagation.BasicTxTest   : 외부 트랜잭션 롤백
+        //2022-10-16 22:47:47.774 DEBUG 1560 --- [           main] o.s.j.d.DataSourceTransactionManager     : Initiating transaction rollback
+        //2022-10-16 22:47:47.774 DEBUG 1560 --- [           main] o.s.j.d.DataSourceTransactionManager     : Rolling back JDBC transaction on Connection [HikariProxyConnection@888942941 wrapping conn0: url=jdbc:h2:mem:1da59437-74fe-4867-baa5-b24a809805f4 user=SA]
+    }
+
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.rollback(inner); // rollback-only
+
+        log.info("외부 트랜잭션 롤백");
+
+        Assertions.assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        // 내부 논리 트랜잭션이 하나라도 롤백되면 롤백 전용 마크를 표시한다.
+        // 이후 커밋 요청이 들어오면 UnexpectedRollbackException 를 반환한다.
+        // 롤백을 하되 해당 오류를 명확히 사용자나 개발자에게 반환해서 알려주어야한다.
+    }
+
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        // 항상 새로운 트랜잭션을 만드는 설정 : PROPAGATION_REQUIRES_NEW
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+
+        // 내부 트랜잭션이 롤백되더라도 외부에서는 커밋처리된다.
     }
 }

8ea1cc03a19ae4bcc612f356949dafdfc1530687
kdj <88639900+djgohigh@users.noreply.github.com>
2022-11-03 19:05:51 +0900

    동시성문제, 쓰레드로컬

---
+++ Backend/Spring/SpringAdvanced/advanced/src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Thread.sleep;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(2000);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 쓰레드끼리 동일한 전역변수를 사용하면서 값이 공유됨.
+    // 쓰레드로컬을 통한 해결(각 쓰레드 마다의 저장소로 분류해주는 역할)
+}

e35f2ffd383d270de755515300e470293f2f10d3
kdj <88639900+djgohigh@users.noreply.github.com>
2022-11-15 20:29:18 +0900

    템플릿 메서드 적용

---
+++ Backend/Spring/SpringAdvanced/advanced/src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 로그트레이스에 템플릿패턴을 적용
+// 공통된 로직은 템플릿 익명 추상클래스에 두고,
+// 각 비즈니스 단계에 맞는 부분에 call 메서드를 바꿔가며 로그트레이싱
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+        return template.execute("OrderController.request()");
+    }
+}

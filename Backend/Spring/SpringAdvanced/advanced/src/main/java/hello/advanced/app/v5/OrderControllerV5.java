687b50aeb2a1217826b262ac7501adb96f27fe0e
kdj <88639900+djgohigh@users.noreply.github.com>
2022-11-18 19:49:26 +0900

    템플릿 콜백 패턴 - 정리

---
+++ Backend/Spring/SpringAdvanced/advanced/src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    // 서비스처리마다 템플릿생성 X
+    // 템플릿 클래스를 스프링 빈으로 등록하고 관리해도 무방!
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace logTrace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(logTrace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+}

8fa6872ae3d354fd3d2935ec3d11e5069c6ca6b6
kdj <88639900+djgohigh@users.noreply.github.com>
2022-11-21 20:41:10 +0900

    프록시 패턴 - 예제 코드2

--- Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 // @Import(AppV1Config.class)
-@Import(AppV2Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 // 컴포넌트스캔 범위 설정 -> app 패키지 이하
 // -> 서로 다른 방식의 클래스가 담긴 config 패키지가 스캔대상이 되어서는 안됨(학습과정 고려)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
--- Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,6 +1,7 @@
 package hello.proxy.app.v2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,6 +22,7 @@ public class OrderControllerV2 {
         return "ok";
     }
 
+    @GetMapping("/v2/no-log")
     public String noLog() {
         return "ok";
     }

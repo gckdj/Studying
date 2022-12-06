ca60ab5885b5c55ebb9b4e3517142e991920b39f
kdj <88639900+djgohigh@users.noreply.github.com>
2022-12-06 23:46:36 +0900

    직접만든 포인트컷

---
+++ Backend/Spring/SpringAdvanced/proxy-start/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,77 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
+
+public class AdvisorTest {
+
+    @Test
+    void AdvisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointCut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return null;
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String methodName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            // 메서드의 이름이 조건과 일치하는 경우 체크
+            boolean result = method.getName().equals(methodName);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+}
--- Backend/Spring/SpringAdvanced/proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ Backend/Spring/SpringAdvanced/proxy-start/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,18 +1,23 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.DisplayName;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class ProxyFactoryTest {
 
     @Test
-    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    @DisplayName("인터페이스R가 있으면 JDK 동적 프록시 사용")
     void interfaceProxy() {
         ServiceInterface target = new ServiceImpl();
 
@@ -21,13 +26,60 @@ public class ProxyFactoryTest {
         // 프록시에 비즈니스 인스턴스 외 클래스 주입
         proxyFactory.addAdvice(new TimeAdvice());
 
-
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
         log.info("targetClass={}", target.getClass());
         log.info("proxyClass={}", proxy.getClass());
 
         // 프록시 인스턴스에서 구현해놓은 로직실행
         proxy.save();
+
+        // 프록시 객체에 주입되는 클래스에 인터페이스가 있다면 JDK 동적프록시
+        // 없다면 CGLIB 사용
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체 클래스만 있다면 CGLIB 사용")
+    void concreteProxy() {
+
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+
+        // 인터페이스 X 구체클래스 O
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하, 클래스 기반 프록시 사용")
+    void ProxyTargetClass() {
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        // 항상 CGLIB 를 기준으로 생성하는 설정
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
 }
 

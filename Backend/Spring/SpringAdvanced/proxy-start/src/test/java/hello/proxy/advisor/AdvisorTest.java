45a39d9a71c87ff1d607f08332531cbd1569bb3f
kdj <88639900+djgohigh@users.noreply.github.com>
2022-12-07 23:34:04 +0900

    ProxyFactory 적용

--- Backend/Spring/SpringAdvanced/proxy-start/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ Backend/Spring/SpringAdvanced/proxy-start/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,6 +3,7 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.ClassFilter;
@@ -10,9 +11,11 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
+@Slf4j
 public class AdvisorTest {
 
     @Test
@@ -32,8 +35,26 @@ public class AdvisorTest {
     void advisorTest2() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target);
-        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
         proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("save");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
 
         proxy.save();
@@ -49,10 +70,12 @@ public class AdvisorTest {
 
         @Override
         public MethodMatcher getMethodMatcher() {
-            return null;
+            return new MyMethodMatcher();
         }
     }
 
+    // methodName과 일치하는 메서드에는 부가기능을 실행시키고
+    // 일치하지 않는 경우에는 미실행
     static class MyMethodMatcher implements MethodMatcher {
 
         private String methodName = "save";
@@ -61,6 +84,8 @@ public class AdvisorTest {
         public boolean matches(Method method, Class<?> targetClass) {
             // 메서드의 이름이 조건과 일치하는 경우 체크
             boolean result = method.getName().equals(methodName);
+            log.info("포인터컷 호출 method = {} targetClass = {}", method.getName(), targetClass);
+            log.info("포인터컷 호출 result = {}", result);
             return result;
         }
 
@@ -71,7 +96,7 @@ public class AdvisorTest {
 
         @Override
         public boolean matches(Method method, Class<?> targetClass, Object... args) {
-            return false;
+            throw new UnsupportedOperationException();
         }
     }
 }

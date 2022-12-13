cb3c1baec2a2fc9b7058dc0a9742f5744ae3096f
kdj <88639900+djgohigh@users.noreply.github.com>
2022-12-13 23:53:19 +0900

    빈후처리기 정리

--- Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
+++ Backend/Spring/SpringAdvanced/proxy-start/src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -19,6 +19,9 @@ public class BeanPostProcessorConfig {
 
     @Bean
     public PackageLogTracePostProcessor logTracePostProcessor(LogTrace logTrace) {
+        // 프록시를 일일이 만들지 않고 후처리기에서 프록시에서 일괄처리
+        // v1, v2, v3 모두 동작
+        // basePackage를 명시해 애플리케이션 관련 빈들만 프록시 적용 대상이된다.
         return new PackageLogTracePostProcessor("hello.proxy.app", getAdvisor(logTrace));
     }
 

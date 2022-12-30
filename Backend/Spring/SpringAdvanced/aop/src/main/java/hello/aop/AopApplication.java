289c586b8b5b7b87b98578ce13c5e28c49718095
kdj <88639900+gckdj@users.noreply.github.com>
2022-12-30 14:36:07 +0900

    Spring Aop 생성

---
+++ Backend/Spring/SpringAdvanced/aop/src/main/java/hello/aop/AopApplication.java
@@ -0,0 +1,13 @@
+package hello.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringProperties;
+
+@SpringBootApplication
+public class AopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApplication.class, args);
+	}
+}

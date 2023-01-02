b9c45ecc2376797d14746492b193117a5e5f7d26
kdj <88639900+gckdj@users.noreply.github.com>
2023-01-02 22:02:08 +0900

    AspectV4

--- Backend/Spring/SpringAdvanced/aop/src/main/java/hello/aop/order/aop/AspectV2.java
+++ Backend/Spring/SpringAdvanced/aop/src/main/java/hello/aop/order/aop/AspectV2.java
@@ -16,29 +16,9 @@ public class AspectV2 {
     @Pointcut("execution(* hello.aop.order..*(..))")
     private void allOrder() {}
 
-    // 클래스 이름 패턴 : *Service
-    @Pointcut("execution(* *..*Service.*(..))")
-    public void allService() {}
-
     @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature());
         return joinPoint.proceed();
-    }
-
-    // hello.aop.order 패키지, 하위패키지이면서 클래스 이름패턴이 *Service
-    @Around("allOrder() && allService()")
-    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
-        try {
-            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
-            Object result = joinPoint.proceed();
-            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
-            return result;
-        } catch (Exception e) {
-            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
-            throw e;
-        } finally {
-            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
-        }
     }
 }

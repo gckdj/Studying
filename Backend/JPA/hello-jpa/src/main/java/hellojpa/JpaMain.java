142bf5fadcb4edcd3fbf2aefa933fda71a680e9e
kdj <88639900+gckdj@users.noreply.github.com>
2023-03-09 23:02:40 +0900

    영속성 컨텍스트 1

--- Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Backend/JPA/hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -24,9 +25,9 @@ public class JpaMain {
             member.setName("B");
             */
 
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember);
-            findMember.setName("helloJpa");
+            /*Member findMember = em.find(Member.class, 1L);
+            System.out.println("find Member = " + findMember);
+            findMember.setName("helloJpa");*/
 
             // 객체수정 후 commit => database update
             //    /* update
@@ -38,7 +39,44 @@ public class JpaMain {
             //            id=?
 
             // em.persist(member);
+
+            // JPQL
+            // Member 객체대상 쿼리(테이블쿼리 X)
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }*/
+
+            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            // 영속데이터 -> 캐싱 -> JPA 1순위 : 캐싱데이터, 2순위 : DB -> select 쿼리 X
+            // 현업에서 성능적인 이점을 얻기는 어렵지만, 컨셉적으로 이점을 얻을 수 있다.
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            // 영속데이터 O -> 캐싱된 데이터사용
+            Member anotherFindMember = em.find(Member.class, 101L);
+
+            // result = true
+            System.out.println("result = " + (findMember == anotherFindMember));
+
+            // 영속시점 -> 실제 데이터베이스 영향 X
+            // 커밋시점 -> 영향 O
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {

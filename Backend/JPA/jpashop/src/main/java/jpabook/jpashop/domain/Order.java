6d3e0b99bde2aa04557480859180e6a7415b559f
kdj <88639900+gckdj@users.noreply.github.com>
2023-03-15 23:47:14 +0900

    테이블 중심 설계의 문제점

--- Backend/JPA/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Backend/JPA/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -10,8 +10,10 @@ import java.time.LocalDateTime;
 public class Order {
 
     @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
     private Long id;
 
+    @Column(name = "MEMBER_ID")
     private Long memberId;
     private LocalDateTime orderDate;
 
--- Backend/JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Backend/JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,70 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * 엔티티설계시 인덱스나, 제약조건은 명시해주는 것이
+ * 코드 가독성에 도움
+ */
+@Entity
 public class OrderItem {
 
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
 
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    // 대문자 or 소문자 -> 회사관례
+    // 스프링부트 방식 -> order_price
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }

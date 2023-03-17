f515df3fc9c233b33ac5e2df569e7f1a2b79116c
kdj <88639900+gckdj@users.noreply.github.com>
2023-03-17 19:30:56 +0900

    단방향 연관관계

--- Backend/JPA/hello-jpa/src/main/java/hellojpa/Member.java
+++ Backend/JPA/hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
@@ -15,6 +12,36 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    // 1:N 관계시 N에는 @ManyToOne
+    @ManyToOne
+    // 조인 시 매핑할 컬럼 @JoinColumn
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }

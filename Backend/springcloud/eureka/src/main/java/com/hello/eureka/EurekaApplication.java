bb5ebd6d1b8d1ef384e632d334766eb08ff3c2fb
kdj <88639900+gckdj@users.noreply.github.com>
2023-04-13 23:01:54 +0900

    userservice 생성

--- Backend/springcloud/eureka/src/main/java/com/hello/eureka/EurekaApplication.java
+++ Backend/springcloud/eureka/src/main/java/com/hello/eureka/EurekaApplication.java
@@ -2,12 +2,14 @@ package com.hello.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// 유레카 서버등록
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }

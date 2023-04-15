1d51ea34949f5b41af5a33120aaefd3ac16bb41b
kdj <88639900+gckdj@users.noreply.github.com>
2023-04-15 21:28:15 +0900

    스프링 게이트웨이

--- Backend/SpringCloud/test2/src/main/java/com/gateway/test2/Controller.java
+++ Backend/SpringCloud/test2/src/main/java/com/gateway/test2/Controller.java
@@ -1,16 +1,15 @@
-package com.gateway.test1;
+package com.gateway.test2;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/first-service")
-public class ServiceController {
+@RequestMapping("/second-service")
+public class Controller {
 
     @GetMapping("welcome")
     public String welcome() {
-        return "Welcome to the first-test-server.";
+        return "Welcome to the second-test-server.";
     }
 }

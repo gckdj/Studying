66e2b8c382a55c686e62fb708de3e0d49f545171
kdj <88639900+gckdj@users.noreply.github.com>
2023-04-16 22:32:18 +0900

    스프링 게이트웨이 자바소스로

--- Backend/SpringCloud/test1/src/main/java/com/gateway/test1/ServiceController.java
+++ Backend/SpringCloud/test1/src/main/java/com/gateway/test1/ServiceController.java
@@ -1,16 +1,25 @@
 package com.gateway.test1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class ServiceController {
 
     @GetMapping("welcome")
     public String welcome() {
         return "Welcome to the first-test-server.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
     }
 }

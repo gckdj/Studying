e398e07b6d852c45332ec17d5dd034e03bf3d228
kdj <88639900+gckdj@users.noreply.github.com>
2023-04-26 23:57:37 +0900

    find user

--- Backend/SpringCloud/user/src/main/java/com/microservice/user/service/UserService.java
+++ Backend/SpringCloud/user/src/main/java/com/microservice/user/service/UserService.java
@@ -1,7 +1,13 @@
 package com.microservice.user.service;
 
 import com.microservice.user.dto.UserDTO;
+import com.microservice.user.jpa.UserEntity;
 
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
+
+    UserDTO getUserByUserId(String userId);
+
+    // 디비에서 가져온 데이터 바로사용
+    Iterable<UserEntity> getUserByAll();
 }

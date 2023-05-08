011beb64b65e7141fc4e82fe7e34f88c847d774f
kdj <88639900+gckdj@users.noreply.github.com>
2023-05-08 23:56:10 +0900

    userauth

---
+++ Backend/SpringCloud/user/src/main/java/com/microservice/user/security/AuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.microservice.user.security;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservice.user.vo.RequestLogin;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try {
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}

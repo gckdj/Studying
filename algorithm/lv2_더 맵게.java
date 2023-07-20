e04185d04f26738522a63e7c602f7dd72af5836a
kdj <djgohigh@gmail.com>
2023-07-20 23:54:37 +0900

    update

--- algorithm/lv2_더 맵게.java
+++ algorithm/lv2_더 맵게.java
@@ -1,26 +1,71 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-    public static int answer = 0;
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-    public int solution(int[] numbers, int target) {
-        recur(numbers, 0, 0, target);
-        return answer;
-    }
+        for (int s : scoville) {
+            pq.add(s);
+        }
 
-    // DFS -> 재귀
-    public void recur(int[] numbers, int count, int r, int target) {
-        // count: 탐색깊이 체크
-        if (numbers.length > count) {
-            int r_1 = r + numbers[count];
-            int r_2 = r - numbers[count];
-            count++;
-            // 탐색이 완료되지 않았기 때문에 재귀실행
-            recur(numbers, count, r_1, target);
-            recur(numbers, count, r_2, target);
-        } else {
-            // 탐색이 완료되면 타겟과 비교하고 전역변수 += 1
-            if (r == target) {
-                answer += 1;
+        int count = 0;
+
+        while (true) {
+            // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우
+            if (pq.size() == 1 && pq.peek() < K) {
+                return -1;
+                // 모든 음식의 스코빌 지수가 K 이상이 된 경우
+            } else if (pq.peek() >= K) {
+                return count;
+                // 모든 음식의 스코빌 지수가 K 이상이 아니라서 처리가 필요한 경우
+            } else if (pq.size() > 1) {
+                int a = pq.poll();
+                int b = pq.poll();
+                int c = a + (b * 2);
+                pq.add(c);
+                count++;
             }
         }
     }
+    
+    /*public int solution(int[] scoville, int K) {
+        int answer = 0;
+        Arrays.sort(scoville);
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < scoville.length; i++) {
+            list.add(scoville[i]);
+        }
+        
+        boolean flag = false;
+        int count = 0;
+        
+        while (!flag) {
+            int first = list.get(0);
+            int second = list.get(1);
+            int res_sco = first + (second * 2);
+            
+            list.remove(0);
+            list.remove(0);
+            
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) > res_sco) {
+                    list.add(i, res_sco);
+                    break;
+                }
+            }
+            
+            count++;
+            
+            if (list.get(0) >= K) {
+                flag = true;
+            } else if (list.size() == 1 && list.get(0) < K) {
+                flag = true;
+                return -1;
+            }
+        }
+        
+        return count;
+    }*/
 }
